2dee52a2ae6a2fd3bf534731f5be6a48133b18a1
Leonardo Batini <batinileo@gmail.com>
2024-01-20 12:55:41 -0300

    adding signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
-    constructor (private prisma: PrismaService) {}
-    
-    async signup (dto: AuthDto) {
+    constructor(private prisma: PrismaService) { }
+
+    async signup(dto: AuthDto) {
         // generate the password hash
         const hash = await argon.hash(dto.password)
 
         // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                    firstName: dto.firstName,
+                    lastName: dto.lastName
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    createdAt: true,
+                    firstName: true,
+                    lastName: true
+                }
+            })
+            // return the saved user
+            return user
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
             }
-        })
-        // return the saved user
-        return user
-        return {msg: 'I have signup'}
+        }
     }
-
-    signin () {
-        return {msg: 'I have signin'}
+    signin() {
+        return { msg: 'I have signin' }
     }
 
 }
